11b678fce1ec6a3e7cbd75aa5cb68dbdff9fc411
Siddh77 <siddharth.kathiravan07@gmail.com>
2026-03-17 01:18:13 +0530

    Siddharth Added PerformanceComparison

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,47 +1,55 @@
-//version 12.0
+//version 13.0
 //author Siddharth K
-//use case 12: Strategy Pattern for Palindrome Algorithms
+//use case 13: Performance Comparison
 import java.util.Stack;
+
 public class PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
         String input = "level";
 
-        // Choose strategy
+        // Select strategy
         PalindromeStrategy strategy = new StackStrategy();
 
-        // Execute algorithm
+        // Start time
+        long startTime = System.nanoTime();
+
+        // Execute palindrome check
         boolean result = strategy.check(input);
 
+        // End time
+        long endTime = System.nanoTime();
+
+        // Calculate execution time
+        long executionTime = endTime - startTime;
+
+        // Output results
         System.out.println("Input : " + input);
         System.out.println("Is Palindrome? : " + result);
+        System.out.println("Execution Time : " + executionTime + " ns");
     }
 }
 
-interface PalindromeStrategy {
 
+interface PalindromeStrategy {
     boolean check(String input);
 }
 
 
-
 class StackStrategy implements PalindromeStrategy {
-
-
 
     public boolean check(String input) {
 
-        // Create stack to store characters
         Stack<Character> stack = new Stack<>();
 
-        // Push each character of input onto stack
+        // Push characters to stack
         for (char c : input.toCharArray()) {
             stack.push(c);
         }
 
-        // Compare characters by popping from stack
+        // Compare by popping
         for (char c : input.toCharArray()) {
-
             if (c != stack.pop()) {
                 return false;
             }
@@ -50,6 +58,3 @@ class StackStrategy implements PalindromeStrategy {
         return true;
     }
 }
-
-
-
